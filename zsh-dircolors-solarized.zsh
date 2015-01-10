ea537c3c3aff666abef3d4339b7194da910a5bb1
Joel Porquet <joel@porquet.org>
2015-01-10 18:28:18 +0100

    move functions around

--- zsh-dircolors-solarized.zsh
+++ zsh-dircolors-solarized.zsh
@@ -1,15 +1,5 @@
-function setupsolarized ()
-{
-    local _SOLARIZED_THEME="ansi-universal"
-    [[ $# -eq 1 ]] && _SOLARIZED_THEME=$1
-
-    local _SOLARIZED_THEME_PATH="$_ZSH_DIRCOLORS_SOLARIZED_DIR/dircolors.${_SOLARIZED_THEME}"
-    if [[ -f $_SOLARIZED_THEME_PATH ]]; then
-        eval $(dircolors $_SOLARIZED_THEME_PATH)
-    else
-        echo "Could not load theme $_SOLARIZED_THEME_PATH"
-    fi
-}
+# get the directory from when we're sourced
+_ZSH_DIRCOLORS_SOLARIZED_DIR=${ZSH_DIRCOLORS_SOLARIZED_DIR:-${${0:A}:h}/dircolors-solarized}
 
 function lssolarized ()
 {
@@ -19,5 +9,15 @@ function lssolarized ()
     done
 }
 
-# get the directory from when we're sourced
-_ZSH_DIRCOLORS_SOLARIZED_DIR=${ZSH_DIRCOLORS_SOLARIZED_DIR:-${${0:A}:h}/dircolors-solarized}
+function setupsolarized ()
+{
+    local _SOLARIZED_THEME="dircolors.ansi-universal"
+    [[ $# -eq 1 ]] && _SOLARIZED_THEME=$1
+
+    local _SOLARIZED_THEME_PATH="$_ZSH_DIRCOLORS_SOLARIZED_DIR/${_SOLARIZED_THEME}"
+    if [[ -f $_SOLARIZED_THEME_PATH ]]; then
+        eval $(dircolors $_SOLARIZED_THEME_PATH)
+    else
+        echo "Could not load theme $_SOLARIZED_THEME_PATH"
+    fi
+}
